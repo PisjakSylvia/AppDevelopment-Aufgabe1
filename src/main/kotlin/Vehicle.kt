27edb789138d85c492d3d682fadfbf829ce71aef
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-03-19 00:04:37 +0100

    added getWorkshop function and changed postcode variable in Workshop to public

--- src/main/kotlin/Vehicle.kt
+++ src/main/kotlin/Vehicle.kt
@@ -2,7 +2,7 @@ class Vehicle {
     private var id = 0
     private var name =  ""
     private var brand: Brand? = null
-    private var workshops: ArrayList<Workshop> = ArrayList()
+    private var workshops: ArrayList<Workshop>? = ArrayList()
     private var weight = 0
     private var maxPermissibleWeight = 0
     private var speed = 0
@@ -34,5 +34,7 @@ class Vehicle {
         println("Vehicle(id=$id, name='$name', brand=$brand, workshops=$workshops, weight=$weight, maxPermissibleWeight=$maxPermissibleWeight, speed=$speed, maxSpeed=$maxSpeed)")
     }
 
-
+    fun getWorkshop(postcode: int): Workshop?{
+        return workshops.find { it.postcode == postcode }
+    }
 }
--- src/main/kotlin/Workshop.kt
+++ src/main/kotlin/Workshop.kt
@@ -1,7 +1,7 @@
 class Workshop {
     private var name = "";
     private var country = "";
-    private var postcode = 0;
+    var postcode = 0;
     private var city = "";
     private var street = "";
     private var phone = "";
