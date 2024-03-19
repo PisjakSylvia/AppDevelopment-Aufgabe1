7f01b32c003e451cc8ceabdba47c36b4cb5d3d8e
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-03-19 10:47:24 +0100

    added race function and renamed variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 import java.util.*
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
+
 
 
 fun main(args: Array<String>) {
@@ -10,30 +13,31 @@ fun main(args: Array<String>) {
     val workshop = Workshop("CoolCar","Austria",9020,"Klagenfurt","Coolestrasse","0677 000000")
     val workshop2 = Workshop("CoolerCar","Austria",9020,"Klagenfurt","Coolestrasse","0677 000000")
 
-    val vehicle1 = Vehicle(1, "Vehicle1", bmw, arrayListOf(workshop), 1900, 2800, 100.0, 250.0)
-    val vehicle2 = Vehicle(2, "Vehicle2", suzuki, arrayListOf(workshop2), 1600, 2500, 60.0, 200.0)
-    val vehicle3 = Vehicle(3, "Vehicle3", nissan, arrayListOf(workshop, workshop2), 2200, 3100, 90.0, 455.0)
+    val MikesCar = Vehicle(1, "MikesCar", bmw, arrayListOf(workshop), 1900, 2800, 100.0, 250.0)
+    val MyCar = Vehicle(2, "MyCar", suzuki, arrayListOf(workshop2), 1600, 2500, 60.0, 200.0)
+    val OtherVehicle = Vehicle(3, "OtherVehicle", nissan, arrayListOf(workshop, workshop2), 2200, 3100, 90.0, 455.0)
 
-    println("Vehicle 1 Information:")
-    vehicle1.printInfo()
-    println("\nVehicle 2 Information:")
-    vehicle2.printInfo()
-    println("\nVehicle 3 Information:")
-    vehicle3.printInfo()
+    println("MikesCar Information:")
+    MikesCar.printInfo()
+    println("\nMyCar Information:")
+    MyCar.printInfo()
+    println("\nOtherVehicle Information:")
+    OtherVehicle.printInfo()
 
-    println("\nDriving vehicle 1 for 5 kilometers:")
-    vehicle1.drive(5)
-    println("\nDriving vehicle 2 for 3 kilometers:")
-    vehicle2.drive(5)
-    println("\nDriving vehicle 3 for 4 kilometers:")
-    vehicle3.drive(4)
+    println("\nDriving MikesCar for 5 kilometers:")
+    MikesCar.drive(5)
+    println("\nDriving MyCar for 3 kilometers:")
+    MyCar.drive(5)
+    println("\nDriving OtherVehicle for 4 kilometers:")
+    OtherVehicle.drive(4)
 
-    val workshopFor9020 = vehicle1.getWorkshop(9020)
+    val workshopFor9020 = MikesCar.getWorkshop(9020)
     if (workshopFor9020 != null) {
         println("Postcode: ${workshopFor9020.postcode}")
     } else {
         println("Workshop not found for postcode 9020.")
     }
+
     suspend fun race(vehicle: Vehicle, distance: Int) {
         var traveledDistance = 0
         val random = Random()
@@ -46,9 +50,18 @@ fun main(args: Array<String>) {
                 } else {
                     vehicle.brake()
                 }
-                kotlin.coroutines.delay(100) // Adjust the delay as needed (in milliseconds)
+                delay(100) //(in milliseconds)
             }
             traveledDistance++
         }
     }
+
+    val raceDistance = 5 // 5 kilometers race distance
+
+    println("Starting the race!")
+    runBlocking {
+        launch { race(MikesCar, raceDistance) }
+        launch { race(MyCar, raceDistance) }
+    }
+    println("Race finished!")
 }
